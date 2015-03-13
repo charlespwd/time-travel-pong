3f7a2cb8b5715f0e59dc5fb4be2064f8e7332c84
Charles-P. Clermont <charles.pclermont@gmail.com>
2015-03-13 11:54:55 -0400

    With rewind

--- src/tt_pong/core.cljs
+++ src/tt_pong/core.cljs
@@ -1,10 +1,8 @@
 (ns tt-pong.core
   (:refer-clojure :exclude [update])
-  (:require [clojure.browser.repl :as repl]
-            [goog.dom :as dom]))
+  (:require [goog.dom :as dom]))
 
 (def ^:dynamic *world* {:width 600, :height 400, :color "#333"})
-(def game (atom {}))
 
 (defn log [s]
   (.log js/console s))
@@ -13,7 +11,7 @@
   (do (set! (.-fillStyle context) color)
       (.fillRect context x y width height)))
 
-(defn getContext [id]
+(defn get-context [id]
   (.getContext (dom/getElement id) "2d"))
 
 (defn start-animation [f]
@@ -22,8 +20,8 @@
 (defn stop-animation [rafid]
   (.cancelAnimationFrame js/window rafid))
 
-(defn out-of-bounds? [x k]
-  (or (> x (k *world*)) (< x 0)))
+(defn in-bounds? [x k]
+  (or (< x (k *world*)) (> x 0)))
 
 (defprotocol Thing
   (draw [this context])
@@ -44,8 +42,8 @@
   (update [this dt]
     (let [new-x (+ x (* vx (/ dt 1000.0)))
           new-y (+ y (* vy (/ dt 1000.0)))
-          new-vx (if (out-of-bounds? new-x :width) (- vx) vx)
-          new-vy (if (out-of-bounds? new-y :height) (- vy) vy)]
+          new-vx (if (in-bounds? new-x :width) vx (- vx))
+          new-vy (if (in-bounds? new-y :height) vy (- vy))]
       (-> this
           (assoc :x new-x)
           (assoc :y new-y)
@@ -61,42 +59,78 @@
     (-> this
         (assoc :y (+ y (* direction 5))))))
 
-(defprotocol Lifecycled
-  (start [this])
-  (stop [this]))
-
-(defprotocol Animated
-  (animate [this t1 t2]))
-
 (defrecord Game [things context]
   Thing
-  (draw [this ctx]
-    (assoc this :things (doall (map #(draw % ctx) things))))
+  (draw [this _]
+    (assoc this :things (doall (map #(draw % context) things))))
   (update [this dt]
-    (assoc this :things (doall (map #(update % dt) things))))
-  Animated
-  (animate [this old-t timestamp]
-    (let [t1 (or old-t timestamp)
-          dt (- timestamp t1)
-          updated-game (update this dt)]
-      (do (draw updated-game context)
-          (assoc this :rafid
-                 (start-animation
-                   (partial animate updated-game timestamp))))))
-  Lifecycled
-  (start [this]
-    (do (draw this context)
-        (start-animation (partial animate this nil))))
-  (stop [this] nil)) ;; TODO
+    (assoc this :things (doall (map #(update % dt) things)))))
+
+(def app-state (atom {}))
+(def app-history (atom [@app-state]))
+(def animation-frame (atom {}))
+
+(defn update-rafid [rafid]
+  (swap! animation-frame assoc :rafid rafid))
+
+(defn update-app-state [game]
+  (do (swap! app-history conj game)
+      (reset! app-state game)))
+
+(defn undo []
+  (when (> (count @app-history) 1)
+    (swap! app-history pop)
+    (reset! app-state (last @app-history))
+    @app-state))
+
+(defn animate-fwd [game old-t timestamp]
+  (let [t1 (or old-t timestamp)
+        dt (- timestamp t1)
+        updated-game (update game dt)]
+    (do (draw updated-game nil)
+        (update-app-state updated-game)
+        (update-rafid (start-animation
+           (partial animate-fwd updated-game timestamp))))))
+
+(defn animate-bwd [game old-t timestamp]
+  (let [t1 (or old-t timestamp)
+        dt (- timestamp t1)
+        updated-game (undo)]
+    (when-not (empty? updated-game)
+      (draw updated-game nil)
+      (update-rafid (start-animation
+                      (partial animate-bwd updated-game timestamp))))))
+
+(defn start []
+  (do (draw @app-state nil)
+      (start-animation (partial animate-fwd @app-state nil))))
+
+(defn rewind []
+  (do (draw @app-state nil)
+      (start-animation (partial animate-bwd @app-state nil))))
+
+(defn stop []
+  (stop-animation (:rafid @animation-frame)))
+
+(defn reset []
+  (do (stop)
+      (setup)))
 
 (defn setup []
-  (let [context (getContext "game")
+  (let [context (get-context "game")
         world (map->World *world*)
-        ball (->Ball 20 20 290 190 80 80)
+        ball (->Ball 20 20 290 190 100 100)
         player1 (->Player 20 80 10 150 0)
         player2 (->Player 20 80 570 150 0)]
-    (do (reset! game (->Game [world ball player1 player2] context))
-        (js/setTimeout #(start @game) 1000))))
+    (do (reset! app-state (->Game [world ball player1 player2] context))
+        (js/setTimeout #(start) 1000))))
 
 (set! (.-onload js/window) setup)
 
+;; (js/setTimeout #(stop) 3000)
+;; (js/setTimeout #(rewind) 3000)
+(js/setTimeout #(reset) 3000)
+
+;; (js/setTimeout #(log @app-history) 7000)
+
+;; (js/setTimeout #(start) 10000)

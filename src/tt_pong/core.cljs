1c2611ed5c60f481a14c6332ef641b2ea31cb597
Charles-P. Clermont <charles.pclermont@gmail.com>
2015-03-15 09:42:43 -0400

    Add Score and stuff

--- src/tt_pong/core.cljs
+++ src/tt_pong/core.cljs
@@ -4,16 +4,32 @@
 
 (def ^:dynamic *world* {:width 600, :height 400, :color "#333"})
 (def ^:dynamic *max-bounce-angle* (* (.-PI js/Math) 0.41)) ;; ~~ 75degrees
-(def ^:dynamic *panel-height* 80)
+(def ^:dynamic *panel-height* 100)
 
-(declare reset)
+(def app-state (atom {}))
+(def app-history (atom [@app-state]))
+(def animation-frame (atom {}))
+(def score (atom [0 0]))
+
+(declare reset stop setup)
+
+(defn reset []
+  (do (reset! app-state {})
+      (reset! app-history [@app-state])
+      (stop)
+      (setup)))
 
 (defn log [s]
   (.log js/console s))
 
-(defn drawRect [context color x y width height]
+(defn draw-rect [context color x y width height]
   (do (set! (.-fillStyle context) color)
       (.fillRect context x y width height)))
+
+(defn draw-text [context color font text x y]
+  (do (set! (.-fillStyle context) color)
+      (set! (.-font context) font)
+      (.fillText context text x y)))
 
 (defn get-context [id]
   (.getContext (dom/getElement id) "2d"))
@@ -32,7 +48,7 @@
 (defrecord World [width height color]
   Thing
   (draw [this context]
-    (do (drawRect context color 0 0 width height)
+    (do (draw-rect context color 0 0 width height)
         this))
   (update [this dt] this)
   (center [this]
@@ -42,7 +58,7 @@
 (defrecord Ball [width height x y vx vy]
   Thing
   (draw [this context]
-    (do (drawRect context "#fff" x y width height)
+    (do (draw-rect context "#fff" x y width height)
         this))
   (update [this dt]
     (let [new-x (+ x (* vx (/ dt 1000.0)))
@@ -58,7 +74,7 @@
 (defrecord Player [width height x y direction]
   Thing
   (draw [this context]
-    (do (drawRect context "#fff" x y width height)
+    (do (draw-rect context "#fff" x y width height)
         this))
   (update [this dt]
     (-> this
@@ -92,8 +108,8 @@
 ;;; to be cheated...
 (defn update-ball [[world ball p1 p2 :as things]]
   (let [[bx by] (center ball)
-        p1x (:x p1)
-        p2x (:x p2)]
+        [p1x] (center p1)
+        [p2x] (center p2)]
     (cond
       (and (<= bx p1x) (<= bx ))
         (bounce-x (center ball) (center p1) 1)
@@ -104,26 +120,15 @@
         (bounce-y (center ball) -1)
       :default ball)))
 
-(defn score? [{[_ ball] :things}]
-  (let [[bx] (center ball)]
-    (not (< 0 bx (:width *world*)))))
-
 (defrecord Game [things context]
   Thing
   (draw [this _]
     (assoc this :things (doall (map #(draw % context) things))))
   (update [this dt]
-    (if-not (score? this)
-      (-> this
-          (assoc-in [:things 1] (update-ball things))
-          (update-in [:things]
-                     #(into [] (doall (map (fn [t] (update t dt)) %)))))
-      this
-      )))
-
-(def app-state (atom {}))
-(def app-history (atom [@app-state]))
-(def animation-frame (atom {}))
+    (-> this
+        (assoc-in [:things 1] (update-ball things))
+        (update-in [:things]
+                   #(into [] (doall (map (fn [t] (update t dt)) %)))))))
 
 (defn update-rafid [rafid]
   (swap! animation-frame assoc :rafid rafid))
@@ -131,6 +136,26 @@
 (defn update-app-state [game]
   (do (swap! app-history conj game)
       (reset! app-state game)))
+
+(defn score? [{[_ ball] :things}]
+  (let [[bx] (center ball)]
+    (not (< 0 bx (:width *world*)))))
+
+(defrecord Score [x y color font]
+  Thing
+  (draw [this context]
+    (let [text (str (first @score) " - " (second @score))]
+     (draw-text context color font text x y)))
+  (update [this _]
+    this)
+  (center [this]
+    [x y]))
+
+(defn update-score [{[_ ball] :things}]
+  (let [[bx]        (center ball)
+         p1-scored? (neg? (- (:width *world*) bx))
+         index      (if p1-scored? 0 1)]
+    (swap! score update-in [index] inc)))
 
 (defn undo []
   (when (> (count @app-history) 1)
@@ -144,49 +169,56 @@
         updated-game (update game dt)]
     (do (draw updated-game nil)
         (update-app-state updated-game)
-        (update-rafid (start-animation
-           (partial animate-fwd updated-game timestamp))))))
+        (if-not (score? updated-game)
+          (update-rafid (start-animation
+           (partial animate-fwd updated-game timestamp)))
+          (do (update-score updated-game)
+              (reset))))))
 
 (defn animate-bwd [game old-t timestamp]
   (let [t1 (or old-t timestamp)
         dt (- timestamp t1)
         updated-game (undo)]
-    (when-not (empty? updated-game)
-      (draw updated-game nil)
+    (if-not (empty? updated-game)
+      (do (draw updated-game nil)
+          (update-rafid (start-animation
+                          (partial animate-bwd updated-game timestamp))))
       (update-rafid (start-animation
-                      (partial animate-bwd updated-game timestamp))))))
-
-(defn start []
-  (do (draw @app-state nil)
-      (start-animation (partial animate-fwd @app-state nil))))
+                      (partial animate-fwd game timestamp))))))
 
 (defn stop []
   (stop-animation (:rafid @animation-frame)))
 
-(defn setup []
-  (let [context (get-context "game")
-        world (map->World *world*)
-        ball (->Ball 20 20 290 190 200 100)
-        player1 (->Player 20 80 10 150 0)
-        player2 (->Player 20 80 570 150 0)]
-    (do (reset! app-state (->Game [world ball player1 player2] context))
-        (js/setTimeout #(start) 1000))))
-
-(defn reset []
+(defn start []
   (do (stop)
-      (setup)))
+      (draw @app-state nil)
+      (start-animation (partial animate-fwd @app-state nil))))
 
 (defn rewind []
   (do (stop)
       (draw @app-state nil)
       (start-animation (partial animate-bwd @app-state nil))))
 
+(defn speed [v]
+  (let [theta (rand (* 2 (.-PI js/Math)))]
+    [(* v (.cos js/Math theta))
+     (* v (.sin js/Math theta))]))
+
+(defn setup []
+  (let [context (get-context "game")
+        world (map->World *world*)
+        ball (apply ->Ball 20 20 290 190 (speed 400))
+        player1 (->Player 20 80 10 150 0)
+        player2 (->Player 20 80 570 150 0)
+        gamescore (->Score (- 300 60) 50 "#fff" "40px Courier")]
+    (do (reset! app-state (->Game [world ball player1 player2 gamescore] context))
+        (js/setTimeout #(start) 500))))
+
 (set! (.-onload js/window) setup)
 
 ;; (js/setTimeout #(stop) 3000)
-;; (js/setTimeout #(rewind) 3000)
+;; (js/setTimeout #(rewind) 4000)
+;; (js/setTimeout #(start) 5000)
 ;; (js/setTimeout #(reset) 4000)
-
 ;; (js/setTimeout #(log @app-history) 7000)
-
 ;; (js/setTimeout #(start) 10000)

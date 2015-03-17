3970b728d39c5548777d5a0efd55cb12c52dd1d8
Charles-P. Clermont <charles.pclermont@gmail.com>
2015-03-17 11:59:26 -0400

    Refactor constructing and functions

--- src/tt_pong/core.cljs
+++ src/tt_pong/core.cljs
@@ -3,8 +3,10 @@
   (:require [goog.dom :as dom]))
 
 (def ^:dynamic *world* {:width 600, :height 400, :color "#333"})
-(def ^:dynamic *max-bounce-angle* (* (.-PI js/Math) 0.41)) ;; ~~ 75degrees
-(def ^:dynamic *panel-height* 100)
+(def max-bounce-angle (* (.-PI js/Math) 0.41)) ;; ~~ 75degrees
+(def player-width 20)
+(def player-height 100)
+(def ball-size 20)
 
 (declare restart stop setup)
 
@@ -59,14 +61,21 @@
 (defn speed [{:keys [vx vy]}]
   (sqrt (+ (* vx vx) (* vy vy))))
 
-(defn bounce-y [[bx by {:keys [vy y] :as ball}] dir]
+(defprotocol Thing
+  (draw [this context])
+  (update [this dt])
+  (center [this]))
+
+(defn bounce-y [{:keys [vy y] :as ball} dir]
   (-> ball
       (assoc :vy (* dir (abs vy)))
       (assoc :y (+ y dir))))
 
-(defn bounce-x [[bx by {:keys [x] :as ball}] [px py player] dir]
-  (let [relative-y (/ (- by py) (/ *panel-height* 2))
-        theta (* relative-y *max-bounce-angle*)
+(defn bounce-x [{:keys [x] :as ball} player dir]
+  (let [[bx by] (center ball)
+        [px py] (center player)
+        relative-y (/ (- by py) (/ player-height 2))
+        theta (* relative-y max-bounce-angle)
         velocity (speed ball)
         ballvx (* velocity (cos theta))
         ballvy (* velocity (sin theta))]
@@ -83,21 +92,16 @@
         [p2x] (center p2)]
     (cond
       (<= bx p1x)
-        (bounce-x (center ball) (center p1) 1)
+        (bounce-x ball p1 1)
       (>= bx p2x)
-        (bounce-x (center ball) (center p2) -1)
+        (bounce-x ball p2 -1)
       (<= by 0)
-        (bounce-y (center ball) 1)
+        (bounce-y ball 1)
       (>= by (:height *world*))
-        (bounce-y (center ball) -1)
+        (bounce-y ball -1)
       :default ball)))
 
 ;;; Records and Protocols
-
-(defprotocol Thing
-  (draw [this context])
-  (update [this dt])
-  (center [this]))
 
 (defrecord World [width height color]
   Thing
@@ -168,14 +172,14 @@
   (let [[bx] (center ball)]
     (not (< 0 bx (:width *world*)))))
 
-(defn score! [game]
-  (do (update-score! game) (restart)))
-
 (defn update-score! [{[_ ball] :things}]
   (let [[bx]        (center ball)
          p1-scored? (neg? (- (:width *world*) bx))
          index      (if p1-scored? 0 1)]
     (swap! score update-in [index] inc)))
+
+(defn score! [game]
+  (do (update-score! game) (restart)))
 
 (defn update-app-state! [game]
   (do (swap! app-history conj game)
@@ -226,18 +230,36 @@
 
 (defn setup []
   (let [context (get-context "game")
+
         world (map->World *world*)
-        ball (apply ->Ball 20 20 290 190 (random-velocity 200))
-        player1 (->Player 20 80 10 150 0)
-        player2 (->Player 20 80 570 150 0)
+
+        [vx vy] (random-velocity 200)
+        [xc yc] (center world)
+        yoffset (- yc (/ player-height 2))
+
+        ball (map->Ball {:width  ball-size
+                         :height ball-size
+                         :x 290, :y 190
+                         :vx vx, :vy vy})
+
+        player1 (map->Player {:width  player-width
+                              :height player-height
+                              :x 10, :y yoffset
+                              :direction 0})
+
+        player2 (map->Player {:width  player-width
+                              :height player-height
+                              :x 570, :y yoffset
+                              :direction 0})
+
         gamescore (->Score (- 300 60) 50 "#fff" "40px Courier")]
     (do (reset! app-state (->Game [world ball player1 player2 gamescore] context))
         (timeout #(start) 500))))
 
 (set! (.-onload js/window) setup)
 
-#_(timeout #(stop) 3000)
-#_(timeout #(rewind) 5000)
-#_(timeout #(start) 6000)
-#_(timeout #(restart) 4000)
-#_(timeout #(start) 10000)
+;; #_(timeout #(stop) 3000)
+;; #_(timeout #(rewind) 5000)
+;; #_(timeout #(start) 6000)
+;; #_(timeout #(restart) 4000)
+;; #_(timeout #(start) 10000)
